60e53002c78dc6d34312ea231f9fe82c5aa2d40a
Thomas Coldwell <31568400+thomas-coldwell@users.noreply.github.com>
2025-03-24 13:45:21 +0000

    fix: embed midi pitch value in noteheads

--- src/VexFlowPatch/src/notehead.js
+++ src/VexFlowPatch/src/notehead.js
@@ -236,6 +236,18 @@ export class NoteHead extends Note {
     if (this.style) {
       this.applyStyle(ctx);
     }
+    
+    // Embed the `midiPitch` as a custom attribute there (required for matching purposes)
+    this.setAttribute(
+      'el',
+      this.context.openGroup(
+        'notehead',
+        this.getAttribute('id'),
+        {
+          midiPitch: this.midi_pitch
+        }
+      )
+    );
 
     if (this.note_type === 's') {
       const staveSpace = this.stave.getSpacingBetweenLines();
